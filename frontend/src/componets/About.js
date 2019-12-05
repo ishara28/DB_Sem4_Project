45a713d648e99e99295ece73233e0984db6b4e34
Aanka25 <asankar.17@cse.mrt.ac.lk>
2019-12-05 16:54:12 +0530

    fs

---
+++ frontend/src/componets/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum The UI/UX Design Specialization brings a design-centric
+        approach to user interface and user experience design, and offers
+        practical, skill-based instruction centered around a visual
+        communications perspective, rather than on one focused on marketing or
+        programming alone. In this sequence of four courses, you will summarize
+        and demonstrate all stages of the UI/UX development process, from user
+        research to defining a project’s strategy, scope, and information
+        architecture, to developing sitemaps and wireframes. You’ll learn
+        current best practices and conventions in UX design and apply them to
+        create effective and compelling screen-based experiences for websites or
+        apps.
+      </p>
+    </div>
+  );
+};
+
+export default About;
