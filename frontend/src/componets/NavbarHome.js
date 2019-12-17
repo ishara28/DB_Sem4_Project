d1dcd35c439d5b5c7e977d5c218d9547fa50704e
Aanka25 <asankar.17@cse.mrt.ac.lk>
2019-12-17 20:47:33 +0530

    Sign up register links added

--- frontend/src/componets/NavbarHome.js
+++ frontend/src/componets/NavbarHome.js
@@ -1,45 +1,30 @@
-import { Link, NavLink, withRouter,Route } from "react-router-dom";
+import { Link, NavLink, withRouter, Route } from "react-router-dom";
 
 import React, { Component } from "react";
 
-const NavbarHome =(props)=> {
-  
+const NavbarHome = props => {
+  return (
+    <nav className=" cyan darken-4">
+      <div className="container">
+        <a className="brand-logo">C online Market</a>
+        <ul className="right">
+          <li>
+            <Link to="/signup" className="waves-effect waves-light btn">
+              Sign up
+            </Link>
+          </li>
+          <li>{/* <Link to="/">7788</Link> */}</li>
 
- 
-    return (
-      <nav className=" cyan darken-4">
-        <div className="container">
-          <a className="brand-logo">C online Market</a>
-          <ul className="right">
-            <li>
-              <button
-                
-                className="waves-effect waves-light btn"
-                style={{ margin: "20px" }}
-              >
-                SignIn
-              </button>
-              
-            </li>
-            <li>
-            {/* <Link to="/">7788</Link> */}
-            </li>
-
-            <li>
-              <button
-               
-                className="waves-effect waves-light btn"
-                style={{ margin: "20px" }}
-              >
-                Register
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-
+          <li>
+            <Link to="/register" className="waves-effect waves-light btn">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default NavbarHome;
 
