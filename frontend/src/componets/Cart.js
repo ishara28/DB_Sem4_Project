d1e1920c04301aa9afe7b4025c92eabfa5db6eb7
Aanka25 <asankar.17@cse.mrt.ac.lk>
2019-12-19 09:47:59 +0530

    Register form finished

--- frontend/src/componets/Cart.js
+++ frontend/src/componets/Cart.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 export class Cart extends Component {
+    state={
+        products:[],
+        product:null,
+        qty:1
+    }
+
+    componentDidMount() {
+        let id = this.props.match.params.product_id;
+        console.log("pased",id)
+        
+        // Axios.get("http://localhost:5000/products/showcase/" + id).then(res => {
+        //   this.setState({
+        //     post: res.data
+        //   });
+        //   console.log(res);
+        // });
+      }
+
     render() {
         return (
             <div>
-                
+    <h1>{this.state.product}</h1>
             </div>
         )
     }
