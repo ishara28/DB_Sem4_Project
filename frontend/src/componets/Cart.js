f9485afbdde3bbaaca1ef5507fb622ff5d447668
Aanka25 <asankar.17@cse.mrt.ac.lk>
2019-12-20 14:37:26 +0530

    Products didnnt work

--- frontend/src/componets/Cart.js
+++ frontend/src/componets/Cart.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Item from "./Item";
 
 export class Cart extends Component {
-    state={
-        products:[],
-        product:null,
-        qty:1
-    }
+  state = {
+    products: [],
+    product: null,
+    qty: 1
+  };
 
-    componentDidMount() {
-        let id = this.props.match.params.product_id;
-        console.log("pased",id)
-        
-        // Axios.get("http://localhost:5000/products/showcase/" + id).then(res => {
-        //   this.setState({
-        //     post: res.data
-        //   });
-        //   console.log(res);
-        // });
-      }
+  componentDidMount() {
+//     var id = this.props.match.params.product_id;
+//     console.log("pased", id);
 
-    render() {
-        return (
-            <div>
-    <h1>{this.state.product}</h1>
-            </div>
-        )
-    }
+    // Axios.get("http://localhost:5000/products/showcase/" + id).then(res => {
+    //   this.setState({
+    //     post: res.data
+    //   });
+    //   console.log(res);
+    // });
+    console.log("fk")
+  }
+
+  render() {
+    return (
+      <div className="card-content col-4">
+        <Item/>
+      </div>
+    );
+  }
 }
 
-export default Cart
+export default Cart;
